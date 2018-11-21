c33c42648c7250de4c12256245875cd4e93bc26f
Russell Hofvendahl <rhofvendahl@gmail.com>
2018-11-20 21:09:33 -0800

    add chat ui, socket io

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template
-# from flask_socketio import SocketIO, emit
-# import os
-# import dialogflow_v2 as dialogflow
-
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'secret!'
-# socketio = SocketIO(app)
+
+from flask_socketio import SocketIO, emit
+socketio = SocketIO(app)
+
+# import os
+# import dialogflow_v2 as dialogflow
 #
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'keys/service_account_key.json'
 # session_client = dialogflow.SessionsClient()
 # PROJECT_ID = 'loopy-nl-agent-1-0'
-#
+
+import spacy
+print('loading en_core_web_md...')
+nlp = spacy.load('en_core_web_sm')
+print('done')
+
 # @app.route('/loopy')
 # def loopy():
 #     return render_template('loopy.html')
@@ -20,15 +26,23 @@ def index():
     return render_template('index.html')
 
 
-# @socketio.on('msg_user', namespace='/chat')
-# def test_message(msg):
-#     session = session_client.session_path(PROJECT_ID, msg['session_id'])
-#     text_input = dialogflow.types.TextInput(text=msg['content'], language_code='en')
-#     query_input = dialogflow.types.QueryInput(text=text_input)
-#     response = session_client.detect_intent(session=session, query_input=query_input)
-#
-#     emit('msg_agent', {'content': response.query_result.fulfillment_text})
+@socketio.on('msg_user', namespace='/chat')
+def test_message(msg):
+    content = msg['content']
+    # session = session_client.session_path(PROJECT_ID, msg['session_id'])
+    # text_input = dialogflow.types.TextInput(text=msg['content'], language_code='en')
+    # query_input = dialogflow.types.QueryInput(text=text_input)
+    # response = session_client.detect_intent(session=session, query_input=query_input)
+
+    # emit('msg_agent', {'content': response.query_result.fulfillment_text})
+
+    emit('msg_agent', {'content': content + content + content})
+
+    doc = nlp(content)
+
+    for token in doc:
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
 
 if __name__ == "__main__":
-    # socketio.run(app)
-    app.run()
+    socketio.run(app)
+    # app.run()
---
+++ static/js/factorGraph.js
@@ -0,0 +1,39 @@
+function Chat(){
+  var self = this
+  self.log = document.getElementById('chat_log')
+  self.input = document.getElementById('chat_input')
+  self.socket = io.connect('http://' + document.domain + ':' + location.port + '/chat');
+  // self.session_id = Math.ceil(Math.random()*Math.pow(16, 8)).toString(16)
+
+  self.logMessage = function(content, sender){
+    var msg_dom = document.createElement('div')
+    msg_dom.innerHTML = content
+    msg_dom.classList.add('msg', 'msg_' + sender)
+    self.log.appendChild(msg_dom)
+    self.log.scrollTop = self.log.scrollHeight;
+  }
+
+  self.input.onkeydown = function(event){
+    if (event.key == 'Enter'){
+      content = self.input.value
+      self.logMessage(content, 'user')
+      self.input.value = ''
+
+      self.socket.emit('msg_user', {
+        content: content,
+        session_id: self.session_id
+      });
+    }
+  };
+
+  self.socket.on('msg_agent', function(msg){
+    self.logMessage(msg.content, 'agent')
+  });
+
+  const displacy = new displaCy('127.0.0.1:5000', {
+    container: '#displacy',
+    format: 'spacy',
+    distance: 300,
+    offsetX: 100
+  });
+}
